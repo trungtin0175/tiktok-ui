141278acf4403d57ee0931655512842883830e68
trungtin0175 <trungtin0175@gmail.com>
2023-03-20 22:39:03 +0700

    Cấu hình Router Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import logo from "./logo.svg";
-import Button from "~/components/Button";
-import "./App.css";
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaltLayout } from './components/Layout';
 function App() {
-  return (
-    <div className="App">
-      <Button />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* <Route path="/" element={<Home />} />
+                    <Route path="/following" element={<Following />} /> */}
+                    {publicRoutes.map((routes, index) => {
+                        // const Layout = routes.layout === null ? Fragment : DefaltLayout;
+                        let Layout = DefaltLayout;
+                        if (routes.layout) {
+                            Layout = routes.layout;
+                        } else if (routes.layout === null) {
+                            Layout = Fragment;
+                        }
+                        const Page = routes.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={routes.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,2 @@
+export { default as DefaltLayout } from './DefaltLayout';
+export { default as HeaderOnly } from './HeaderOnly';
